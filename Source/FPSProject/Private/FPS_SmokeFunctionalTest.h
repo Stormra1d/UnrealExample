e3ce1649edd19391bcae907ba60d963e98503145
Stormra1d <lennschlott@gmail.com>
2025-09-06 14:45:01 +0200

    fix: typo

--- Source/FPSProject/Private/FPS_SmokeFunctionalTest.h
+++ Source/FPSProject/Private/FPS_SmokeFunctionalTest.h
@@ -1,9 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
-#include "CoreMinimal.h"
 #if WITH_EDITOR
+#include "CoreMinimal.h"
 #include "FunctionalTest.h"
 #include "FPS_SmokeFunctionalTest.generated.h"
 
